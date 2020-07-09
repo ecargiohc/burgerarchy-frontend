b8ef0212eaf6b19a705f5183b0314b8aed74513f
github username <therealgracechoi@gmail.com>
2020-07-08 17:35:39 -0700

    added review form

---
+++ src/Review/ReviewForm.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class ReviewForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: "", 
+            description: "",
+            score: 0,
+            restaurant_id: 0
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <p>REVIEW FORM</p>
+            </div>
+        )
+    }
+
+}
+export default ReviewForm;
