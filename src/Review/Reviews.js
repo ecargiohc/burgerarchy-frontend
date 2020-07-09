5cbf73f66d27692edeb4497793b26a45c0855d14
github username <therealgracechoi@gmail.com>
2020-07-08 17:37:06 -0700

    added reviews.js component

---
+++ src/Review/Reviews.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+
+const REVIEW_URL = `http://localhost:3001/api/v1/reviews`
+
+class Reviews extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reviews: []
+        }
+    }
+
+    getReviews() {
+        fetch(REVIEW_URL)
+        .then(response => response.json())
+        .then(res_data => {
+console.log(res_data);
+            this.setState({
+                reviews: res_data
+            })
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <ul id="reviews-list">
+                    {this.state.reviews.map((r) => {
+                        <Review 
+                        key={r.id} 
+                        title={r.title}
+                        description={r.description}
+                        score={r.score}
+                        restaurant={r.restaurant}/>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
